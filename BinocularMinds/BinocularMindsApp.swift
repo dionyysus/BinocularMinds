f8b8e19204c49d0b94343545b4cfbbd24dbb3562
Gizem <gizemcsk1@gmail.com>
2025-09-22 17:17:38 +0200

    project created

---
+++ BinocularMinds/BinocularMindsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinocularMindsApp.swift
+//  BinocularMinds
+//
+//  Created by Gizem Coskun on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BinocularMindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
