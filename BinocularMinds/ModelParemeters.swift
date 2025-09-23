a844cd517c058df60d659c9fc2963a8371811b77
Juliesg02 <84135062+Juliesg02@users.noreply.github.com>
2025-09-23 16:59:23 +0200

    ModelParameter creation

---
+++ BinocularMinds/ModelParemeters.swift
@@ -0,0 +1,17 @@
+//
+//  ModelParemeters.swift
+//  BinocularMinds
+//
+//  Created by Julio Enrique Sanchez Guajardo on 23/09/25.
+//
+
+import Foundation
+
+@Observable
+class ModelParameters {
+    var scaleValue: CGFloat = 0.2
+    var angleDegreeX: Float = 0.0
+    var angleDegreeY: Float = 0.0
+    var angleDegreeZ: Float = 0.0
+    var selectedModel: String = "Can"
+}
